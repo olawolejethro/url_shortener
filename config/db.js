c6bf13075fa63826e96b2b00e7819b206262c77b
Olawole Jethro <kunlekzi2@gmail.com>
2023-06-07 23:07:56 +0100

    progess

--- config/db.js
+++ config/db.js
@@ -7,9 +7,11 @@ const DB_CONNECTION_URL = process.env.MONGO_DB_URL;
 const dbConnection = async () => {
   try {
     await mongoose.connect(DB_CONNECTION_URL, {
+
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log("connected to server successfully")
   } catch (err) {
     console.error(err.message);
     process.exit(1);
